c543b903175a50b86dd3891078106481cb2d48ff
Kinnell <kinnell@gmail.com>
2014-03-21 10:48:09 -0400

    Added confirmable to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
    validates_format_of :email, :with => /\A[\w+\-.]+@eclinicalworks.com/i, :allow_blank => true, :message=>"needs to be an eClinicalWorks email."
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319181540) do
+ActiveRecord::Schema.define(version: 20140320203710) do
 
   create_table "products", force: true do |t|
     t.string   "name",       null: false
@@ -38,8 +38,12 @@ ActiveRecord::Schema.define(version: 20140319181540) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
