7b9a0021d9089625f5177cfffc940caf5f538676
Kinnell <kinnell@gmail.com>
2014-11-19 17:46:01 -0500

    Refactored & formatted Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,7 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:calendar]
-
-  def home
-  end
+  before_action :authenticate_user!
 
   def calendar
-    @tasks = Task.all.due.order("due_date")
-    @tasks_by_date = @tasks.group_by {|t| t.due_date.to_date}
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-  end
-
-   def myCalendar
     @tasks = current_user.tasks.due.order("due_date")
     @tasks_by_date = @tasks.group_by {|t| t.due_date.to_date}
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
--- config/routes.rb
+++ config/routes.rb
@@ -15,34 +15,14 @@ Apollo::Application.routes.draw do
 
   resources :print_jobs, only: [:index, :create, :edit, :update, :destroy]
 
-  authenticated :user do
-    root :to => "projects#index", as: :authenticated_root
-  end
+  root to: "projects#index"
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  get "calendar", to: "pages#calendar"
+  get "changelog", to: "pages#changelog"
 
-  get '/projects', to: "projects#index"
-  get '/projects/new', to: "projects#new"
-
-  get '/tasks', to: "tasks#index"
-
-  get "notes/index"
-  get "notes/new"
-
-  get '/users', to: "users#index"
-
-  get 'calendar', to: "pages#calendar"
-  get 'myCalendar', to: "pages#myCalendar"
-  get 'changelog', to: "pages#changelog"
-
-  match '/projects/:id/complete' => "projects#complete", :as => :complete_project, via: [:post]
-  match '/projects/:id/uncomplete' => "projects#uncomplete", :as => :uncomplete_project, via: [:post]
-
-  match '/projects/:id/toggle_starred' => "projects#toggle_starred", :as => :toggle_starred_project, via: [:post]
-
-  match '/tasks/:id/toggle_starred' => "tasks#toggle_starred", :as => :toggle_starred_task, via: [:patch]
-
+  match "/projects/:id/complete" => "projects#complete", :as => :complete_project, via: [:post]
+  match "/projects/:id/uncomplete" => "projects#uncomplete", :as => :uncomplete_project, via: [:post]
+  match "/projects/:id/toggle_starred" => "projects#toggle_starred", :as => :toggle_starred_project, via: [:post]
+  match "/tasks/:id/toggle_starred" => "tasks#toggle_starred", :as => :toggle_starred_task, via: [:patch]
 
 end
