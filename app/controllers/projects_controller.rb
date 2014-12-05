0d9187984313f39bbbd64ab1b6d243da80726bce
Kinnell <kinnell@gmail.com>
2014-12-05 16:50:15 -0500

    Made x on comment hover only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,9 @@ class ProjectsController < ApplicationController
     @q = Project.all.search(params[:q])
     @q.users_name_eq = current_user.name unless params[:q] && params[:q][:users_name_eq]
     @projects = @q.result(distinct: true)
+
+    @projects = @projects.incomplete unless params[:q] && params[:q][:status_eq]
+
     @projects = @projects.priority_sort unless params[:q] && params[:q][:s]
 
     respond_to do |format|
