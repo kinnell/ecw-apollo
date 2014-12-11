0f7ba783fa928c67bf23f4f8869557fe64c35191
Kinnell <kinnell@gmail.com>
2014-12-11 15:12:30 -0500

    Added User active attribute & accordingly modified status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,29 @@
 class User < ActiveRecord::Base
-	devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-   validates_format_of :email, with: /\A[\w+\-.]+@eclinicalworks.com/i, allow_blank: true, message: "needs to be an eClinicalWorks email."
+  validates_format_of :email, with: /\A[\w+\-.]+@eclinicalworks.com/i, allow_blank: true, message: "needs to be an eClinicalWorks email."
 
-   has_many :assignments, dependent: :destroy
-   has_many :projects, through: :assignments
+  has_many :assignments, dependent: :destroy
+  has_many :projects, through: :assignments
 
-   has_many :comments
-   has_many :tasks
+  has_many :comments
+  has_many :tasks
 
-   has_attached_file :profile_photo, styles: { medium: "200x200#", thumb: "75x75#", small: "50x50", tiny: "25x25" }, default_url: "turtle75.jpg"
+  scope :active, -> { where(active: true) }
 
-end
+  has_attached_file :profile_photo, styles: { medium: "200x200#", thumb: "75x75#", small: "50x50", tiny: "25x25" }, default_url: "turtle75.jpg"
+
+  def status
+    if confirmed?
+      if active?
+        "Active"
+      elsif
+        "Inactive"
+      end
+    else
+      "Not Confirmed"
+    end
+  end
+
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204210756) do
+ActiveRecord::Schema.define(version: 20141211170725) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 20141204210756) do
     t.string   "profile_photo_content_type"
     t.integer  "profile_photo_file_size"
     t.datetime "profile_photo_updated_at"
+    t.boolean  "active",                     default: true
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
