5ef44ecfaac9de40253dd4eedd0af54652340b6a
Kinnell <kinnell@gmail.com>
2015-01-08 13:11:58 -0500

    Fixed the issue with project assigned email method
    Needed instance variables for view to render

--- app/mailers/projects_mailer.rb
+++ app/mailers/projects_mailer.rb
@@ -1,9 +1,11 @@
 class ProjectsMailer < ActionMailer::Base
 
   def project_assigned_email(project, user)
-    mail from: "#{project.creator.name} <#{project.creator.email}>",
-      to: "#{user.name} <#{user.email}>",
-      subject: "[eCW apollo] You have been assigned to [#{project.product_name}] #{project.name}"
+    @project = project
+    @user = user
+    mail from: "#{@project.creator.name} <#{@project.creator.email}>",
+      to: "#{@user.name} <#{@user.email}>",
+      subject: "[eCW apollo] You have been assigned to [#{@project.product_name}] #{@project.name}"
   end
 
 end
